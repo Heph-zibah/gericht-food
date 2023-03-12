79f93502b8c8cf6148c15150468d059e1dadace7
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-12 19:48:16 +0100

    added the home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Nav from "./components/Nav";
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="">
      <Nav/>
+     <Routes>
+     <Route path="/" element={ <Home/> } />
+     </Routes>
     </div>
   );
 }
