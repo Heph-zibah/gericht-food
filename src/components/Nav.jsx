7b71fddfdd349bd130d7e411e8b7e7bc49a5b34f
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-12 19:24:01 +0100

    added markup

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { navLinks } from './Data'
+import {FiMenu} from 'react-icons/fi'
+import {FaTimesCircle} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <>
     <header>
         <div className='nav__logo'>
-            <h1>gericht</h1>
+            <h1>Gerícht</h1>
         </div>
         <nav>
             <ul>
@@ -16,6 +19,28 @@ const Nav = () => {
                 })}
             </ul>
         </nav>
+        <div className='r'>
+            <Link to="#">Login/Registration</Link>
+            <Link to="#">Book table</Link>
+            <div className='menu__open'>
+                <FiMenu/>
+            </div>
+        </div>
+        <div className='mobile__nav'>
+            <div className='menu__close'>
+                <FaTimesCircle/>
+            </div>
+            <div>
+                <ul>
+                    {navLinks.map((item) => { 
+                        const {id, url, link} = item
+                        return <li key={id}><a href={url}>{link}</a></li>
+                    })}
+                </ul>
+                <Link to="#">Login/Registration</Link>
+                <Link to="#">Book table</Link>
+            </div>
+        </div>
     </header>
     </>
   )
