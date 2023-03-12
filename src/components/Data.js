578f2ecad3ed9881efdbf1a9966b47304557225d
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-12 19:23:36 +0100

    update data

---
+++ src/components/Data.js
@@ -0,0 +1,27 @@
+export const navLinks = [
+    {
+        id: 1,
+        url: '/home',
+        link: 'home'
+    },
+    {
+        id: 2,
+        url: '/pages',
+        link: 'pages'
+    },
+    {
+        id: 3,
+        url: '/cintact us',
+        link: 'cintact us'
+    },
+    {
+        id: 4,
+        url: '/blog',
+        link: 'blog'
+    },
+    {
+        id: 5,
+        url: '/landing',
+        link: 'landing'
+    },
+]
