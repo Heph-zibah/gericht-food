a4fee459fff1a4b9a52f6cd79ad1530a797d900f
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-13 17:22:13 +0100

    added the markup

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const Error = () => {
+const Error = ({message = 'Something went wrong'}) => {
   return (
-    <div>Error Page</div>
+    <>
+        <div className='error'>
+            <h1>{message}</h1>
+        </div>
+    </>
   )
 }
 
