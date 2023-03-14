6aa121634636a53fb448b4a8a870a6cac9e7b22c
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-14 12:11:45 +0100

    added the file path to hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from '../components/Home/Hero'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <main>
+    <Hero/>
+    </main>
+    </>
   )
 }
 
