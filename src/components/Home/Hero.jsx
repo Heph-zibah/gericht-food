bcf33a7bfe70a20db79e514d36a88f8d306fb148
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-15 11:29:02 +0100

    added the line img

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import heroImg from '../../assets/Hero-img.svg'
 import heroImg2 from '../../assets/hero--img.svg'
+import line from '../../assets/Vector.svg'
 
 const Hero = () => {
   return (
@@ -9,9 +10,10 @@ const Hero = () => {
         <div className='hero__img'>
         <img src={window.innerWidth > 1200 ? heroImg : heroImg2} alt="food" />
         </div>
-        {window.innerWidth > 1200 ? <div className="line"></div> : null}
+        \
         <div className='hero__info'>
             <h5>Chase the new Flavour</h5>
+            <img src={line} alt="line" />
             <h1>The key to Fine dining</h1>
             <p>Sit tellus lobortis sed senectus vivamus molestie. 
                 Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus 
